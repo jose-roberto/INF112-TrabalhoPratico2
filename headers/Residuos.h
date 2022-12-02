5daa2b2c17e524ac3faec1a07e5c7bd9ccdd86c3
JonathanCampos <jonathan.campos@ufv.br>
2022-12-01 21:02:19 -0300

    Adicionando a Classe Residuos

---
+++ headers/Residuos.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Residuos {
+    private:
+        int _id;
+        std::string _nome;
+    public:
+    Residuos(int id, std::string nome);
+
+    void armazenarResiduo();
+};
---
+++ sourcers/Residuos.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../headers/Residuos.h"
+
+Residuos::Residuos(int id, std::string nome){
+    this -> _id = id;
+    this -> _nome = nome;
+}
+
+void Residuos::armazenarResiduo(){}
